10b77f9de7f29516d491e59555b35f2fb4e77f76
Clark Rasmussen <crasmussen@constantcontact.com>
2015-04-05 21:47:23 -0700

    Tree. fold not working yet.

---
+++ exercises/src/main/scala/fpinscala/datastructures/RunTree.scala
@@ -0,0 +1,18 @@
+package fpinscala.datastructures
+
+object RunTree {
+  import Tree._
+
+  val leftTree = Branch(Leaf(3), Leaf(8))
+  val rightTree = Branch(Leaf(2), Leaf(6))
+  val mainTree = Branch(leftTree, rightTree)
+  def main(args: Array[String]): Unit = {
+    println("size        " + size(leftTree))
+    println("size        " + size(rightTree))
+    println("size        " + size(mainTree))
+    println("max         " + maximum(mainTree))
+    println("depth       " + depth(leftTree))
+    println("depth       " + depth(mainTree))
+    println("map         " + map(mainTree)(_ * 10))
+  }
+}
